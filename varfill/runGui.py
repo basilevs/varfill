50d4e5e3dc9f68df2df52f1a8c373b82503a7ab5
Gulevich Vasili <V.V.Gulevich@mail.ru>
2012-10-24 15:44:26 +0700

    Startup connection check

--- varfill/runGui.py
+++ varfill/runGui.py
@@ -1,13 +1,19 @@
 from gui import Gui
 from time import sleep
-from tkinter import Tk, E, W, S, N
+from tkinter import Tk, E, W, S, N, Label
 from control import defaultControl, Control, Mover
+from traceback import print_exc
 
 root=Tk()
 
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-gui = Gui(defaultControl(), root)
-gui.grid(sticky=E+W+S+N)
+try:
+    gui = Gui(defaultControl(), root)
+    gui.grid(sticky=E+W+S+N)
+except OSError as e:
+    print_exc()
+    root.configure(width=200, height=200)
+    Label(root, text="Нет связи").grid()
 root.mainloop() 
