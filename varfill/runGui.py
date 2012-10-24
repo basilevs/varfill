06465d7f46c46e94066a8b8914bb81c344b4623a
Gulevich Vasili <V.V.Gulevich@mail.ru>
2012-10-24 17:58:04 +0700

    Increased font size on initial connection fail message

--- varfill/runGui.py
+++ varfill/runGui.py
@@ -1,21 +1,24 @@
 from gui import Gui
 from time import sleep
-from tkinter import Tk, E, W, S, N, Label
 from control import defaultControl, Control, Mover
 from traceback import print_exc
 import socket
+from tkinter import Tk, E, W, S, N, Label
+from tkinter.font import nametofont
 
 root=Tk()
-
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
-
 try:
-    gui = Gui(defaultControl(), root)
+    control=defaultControl()
+    gui = Gui(control, root)
     gui.grid(sticky=E+W+S+N)
 except (OSError, socket.error) as e:
     print_exc()
     root.configure(width=200, height=200)
     root.bind("<Escape>", lambda x: root.quit())
-    Label(root, text="Нет связи").grid()
+    l=Label(root, text="Нет связи")
+    l.grid()
+    nametofont(l["font"]).config(size=80)
+
 root.mainloop() 
